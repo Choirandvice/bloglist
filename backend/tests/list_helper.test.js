256af5b89bc334be8146ff69d9a9ecf7362e2bba
Choirandvice <ryan.thomas.harrison@gmail.com>
2023-09-19 16:54:00 +0930

    4.4 complete

--- backend/tests/list_helper.test.js
+++ backend/tests/list_helper.test.js
@@ -6,3 +6,73 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  test('of a single blog', () => {
+    expect(listHelper.totalLikes(listWithOneBlog)).toBe(listWithOneBlog[0].likes)
+  })
+
+
+  const listWithManyBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful1',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f7',
+      title: 'Go To Statement Considered Harmful2',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 4,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f6',
+      title: 'Go To Statement Considered Harmful3',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f5',
+      title: 'Go To Statement Considered Harmful4',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 3,
+      __v: 0
+    }
+    ,    {
+      _id: '5a422aa71b54a676234d17f4',
+      title: 'Go To Statement Considered Harmful5',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 10,
+      __v: 0
+    }
+  ]
+  test('of five blogs is correct', () => {
+    expect(listHelper.totalLikes(listWithManyBlogs)).toBe(29)
+  })
+
+  test('of no blogs is zero', () => {
+    expect(listHelper.totalLikes([])).toBe(0)
+  })
+
+})
